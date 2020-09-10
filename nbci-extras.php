beceb401015722ac3b445a7fdd5b40b8a7771e40
Michael Wender <michael@michaelwender.com>
2020-09-10 14:28:16 -0400

    First commit

---
+++ nbci-extras.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Plugin Name:     NBCI Extras
+ * Plugin URI:      PLUGIN SITE HERE
+ * Description:     Site modifications for the NBCI Elementor-powered website.
+ * Author:          Michael Wender
+ * Author URI:      https://mwender.com
+ * Text Domain:     nbci-extras
+ * Domain Path:     /languages
+ * Version:         0.1.0
+ *
+ * @package         Nbci_Extras
+ */
+
+// Your code starts here.
