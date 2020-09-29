4806c745adb5b29022dc2b68a87f61e2a24b36e1
Michael Wender <michael@michaelwender.com>
2020-09-29 10:09:05 -0400

    Adding uber_log()

--- nbci-extras.php
+++ nbci-extras.php
@@ -17,3 +17,20 @@ define( 'NBCI_DIR', plugin_dir_path( __FILE__ ) );
 define( 'NBCI_URL', plugin_dir_url( __FILE__ ) );
 
 require_once( 'lib/fns/site-container.php' );
+
+/**
+ * Enhanced logging.
+ *
+ * @param      string  $message  The log message
+ */
+function uber_log( $message = null ){
+  static $counter = 1;
+
+  $bt = debug_backtrace();
+  $caller = array_shift( $bt );
+
+  if( 1 == $counter )
+    error_log( "\n\n" . str_repeat('-', 25 ) . ' STARTING DEBUG [' . date('h:i:sa', current_time('timestamp') ) . '] ' . str_repeat('-', 25 ) . "\n\n" );
+  error_log( "\n" . $counter . '. ' . basename( $caller['file'] ) . '::' . $caller['line'] . "\n" . $message . "\n---\n" );
+  $counter++;
+}
